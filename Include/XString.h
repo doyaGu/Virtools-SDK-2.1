8be08363fe0760973840222b4236449605b930e5
doyaGu <doyaGu@users.noreply.github.com>
2022-10-09 21:29:05 +0800

    Integrate XString

--- Include/XString.h
+++ Include/XString.h
@@ -1,6 +1,10 @@
 #ifndef XSTRING_H
 #define XSTRING_H
 
+#include <ctype.h>
+#include <stdio.h>
+#include <stdarg.h>
+
 #include "VxMathDefines.h"
 #include "XUtil.h"
 
@@ -139,31 +143,116 @@ public:
     XString() : XBaseString() {}
 
     // Substring Ctor
-    VX_EXPORT XString(const char *iString, const int iLength = 0);
+    XString(const char *iString, const int iLength = 0) : XBaseString()
+    {
+        if (!iString || *iString == '\0')
+        {
+            m_Buffer = NULL;
+            m_Length = 0;
+            return;
+        }
+
+        m_Length = (iLength > 0) ? iLength : (XWORD)strlen(iString);
+        m_Allocated = m_Length + 1;
+        m_Buffer = (char *)VxNew(sizeof(char) * m_Allocated);
+        strncpy(m_Buffer, iString, m_Length);
+    }
 
     // Reserving Ctor
-    VX_EXPORT explicit XString(const int iLength);
+    explicit XString(const int iLength) : XBaseString()
+    {
+        if (iLength == 0)
+        {
+            m_Buffer = NULL;
+            m_Length = 0;
+            return;
+        }
+
+        m_Length = iLength - 1;
+        m_Allocated = iLength;
+        m_Buffer = (char *)VxNew(sizeof(char) * iLength);
+        memset(m_Buffer, 0, m_Allocated);
+    }
 
     // Copy Ctor
-    VX_EXPORT XString(const XString &iSrc);
+    XString(const XString &iSrc) : XBaseString()
+    {
+        if (iSrc.m_Length == 0)
+        {
+            m_Buffer = NULL;
+            m_Length = 0;
+            return;
+        }
+
+        m_Length = iSrc.m_Length;
+        m_Allocated = iSrc.m_Length + 1;
+        m_Buffer = (char *)VxNew(sizeof(char) * m_Allocated);
+        memcpy(m_Buffer, iSrc.m_Buffer, m_Allocated);
+    }
 
     // Copy Ctor
-    VX_EXPORT XString(const XBaseString &iSrc);
+    XString(const XBaseString &iSrc) : XBaseString()
+    {
+        if (iSrc.m_Length == 0)
+        {
+            m_Buffer = NULL;
+            m_Length = 0;
+            return;
+        }
+
+        m_Length = iSrc.m_Length;
+        m_Allocated = iSrc.m_Length + 1;
+        m_Buffer = (char *)VxNew(sizeof(char) * m_Allocated);
+        memcpy(m_Buffer, iSrc.m_Buffer, m_Allocated);
+    }
 
     // Dtor
-    VX_EXPORT ~XString();
+    ~XString() { VxDelete(m_Buffer); }
 
     // operator =
-    VX_EXPORT XString &operator=(const XString &iSrc);
+    XString &operator=(const XString &iSrc)
+    {
+        if (this != &iSrc)
+            Assign(iSrc.m_Buffer, iSrc.m_Length);
+        return *this;
+    }
 
     // operator =
-    VX_EXPORT XString &operator=(const char *iSrc);
+    XString &operator=(const char *iSrc)
+    {
+        if (iSrc)
+        {
+            Assign(iSrc, strlen(iSrc));
+        }
+        else
+        {
+            m_Length = 0;
+            if (m_Buffer)
+                m_Buffer[0] = '\0';
+        }
+        return *this;
+    }
 
     // operator =
-    VX_EXPORT XString &operator=(const XBaseString &iSrc);
+    XString &operator=(const XBaseString &iSrc)
+    {
+        if (this != &iSrc)
+            Assign(iSrc.m_Buffer, iSrc.m_Length);
+        return *this;
+    }
 
     // Create from a string and a length
-    VX_EXPORT XString &Create(const char *iString, const int iLength = 0);
+    XString &Create(const char *iString, const int iLength = 0)
+    {
+        CheckSize(iLength);
+        m_Length = 0;
+        if (iString)
+        {
+            m_Length = iLength;
+            strncpy(m_Buffer, iString, iLength);
+        }
+        return *this;
+    }
 
     /************************************************
     Summary: Conversion to a char*.
@@ -210,13 +299,32 @@ public:
     }
 
     // Format the string sprintf style, ol skool ! yeahhhhh
-    VX_EXPORT XString &Format(const char *iFormat, ...);
+    XString &Format(const char *iFormat, ...)
+    {
+        va_list args;
+        va_start(args, iFormat);
+        char buf[512];
+        int len = vsprintf(buf, iFormat, args);
+        Assign(buf, len);
+        va_end(args);
+        return *this;
+    }
 
     // Capitalize all the characters of the string
-    VX_EXPORT XString &ToUpper();
+    XString &ToUpper()
+    {
+        for (XWORD i = 0; i < m_Length; i++)
+            m_Buffer[i] = toupper(m_Buffer[i]);
+        return *this;
+    }
 
     // Uncapitalize all the characters of the string
-    VX_EXPORT XString &ToLower();
+    XString &ToLower()
+    {
+        for (XWORD i = 0; i < m_Length; i++)
+            m_Buffer[i] = tolower(m_Buffer[i]);
+        return *this;
+    }
 
     // Compare the strings.
     /*************************************************
@@ -278,16 +386,96 @@ public:
     }
 
     // Compare the strings (ignore the case).
-    VX_EXPORT int ICompare(const XBaseString &iStr) const;
+    int ICompare(const XBaseString &iStr) const
+    {
+        if (!m_Length)
+            return -iStr.m_Length; // Null strings
+        if (!iStr.m_Length)
+            return m_Length;
+
+        char *s1 = m_Buffer;
+        char *s2 = iStr.m_Buffer;
+
+        char c1, c2;
+        do
+        {
+            c1 = tolower(*(s1++));
+            c2 = tolower(*(s2++));
+        } while (*s1 != '\0' && (c1 == c2));
+
+        return tolower(*s1) - tolower(*s2);
+    }
 
     ///
     // Complex operations on strings
 
     // removes space ( ' ', '\t' and '\n') from the start and the end of the string
-    VX_EXPORT XString &Trim();
+    XString &Trim()
+    {
+        if (m_Length != 0)
+        {
+            while (isspace(m_Buffer[m_Length - 1]) && m_Length != 0)
+                --m_Length;
+            m_Buffer[m_Length] = '\0';
+
+            if (m_Length != 0)
+            {
+                XWORD i = 0;
+                while (isspace(m_Buffer[i]) && i != m_Length)
+                    ++i;
+                if (i != 0)
+                {
+                    m_Length -= i;
+                    strncpy(m_Buffer, &m_Buffer[i], m_Length + 1);
+                }
+            }
+        }
+        return *this;
+    }
 
     // replaces space ( ' ', '\t' and '\n') sequences by one space
-    VX_EXPORT XString &Strip();
+    XString &Strip()
+    {
+        if (m_Length != 0)
+        {
+            XWORD i;
+            XBOOL wasSpace = FALSE;
+            for (i = 0; i < m_Length; i++)
+            {
+                if (isspace(m_Buffer[i]))
+                {
+                    if (wasSpace)
+                        break;
+                    wasSpace = TRUE;
+                }
+                else
+                {
+                    wasSpace = FALSE;
+                }
+            }
+
+            for (XWORD j = i + 1; j < m_Length; j++)
+            {
+                if (isspace(m_Buffer[j]))
+                {
+                    if (!wasSpace)
+                    {
+                        wasSpace = TRUE;
+                        m_Buffer[i++] = ' ';
+                    }
+                }
+                else
+                {
+                    wasSpace = FALSE;
+                    m_Buffer[i++] = m_Buffer[j];
+                }
+            }
+
+            m_Length = i;
+            m_Buffer[i] = '\0';
+        }
+        return *this;
+    }
 
     // finds a string in the string
     XBOOL Contains(const XBaseString &iStr) const
@@ -296,28 +484,147 @@ public:
     }
 
     // finds a character in the string
-    VX_EXPORT XWORD Find(char iCar, XWORD iStart = 0) const;
+    XWORD Find(char iCar, XWORD iStart = 0) const
+    {
+        if (m_Length != 0)
+        {
+            char *str = strchr(&m_Buffer[iStart], iCar);
+            if (str)
+                return str - m_Buffer;
+            else
+                return -1;
+        }
+        return -1;
+    }
 
     // finds a string in the string
-    VX_EXPORT XWORD Find(const XBaseString &iStr, XWORD iStart = 0) const;
+    XWORD Find(const XBaseString &iStr, XWORD iStart = 0) const
+    {
+        if (m_Length != 0 && iStr.m_Length != 0)
+        {
+            char *str = strstr(&m_Buffer[iStart], iStr.m_Buffer);
+            if (str)
+                return str - m_Buffer;
+            else
+                return -1;
+        }
+        return -1;
+    }
 
     // finds a character in the string
-    VX_EXPORT XWORD RFind(char iCar, XWORD iStart = NOTFOUND) const;
+    XWORD RFind(char iCar, XWORD iStart = NOTFOUND) const
+    {
+        if (m_Length != 0)
+        {
+            XWORD i = (iStart != NOTFOUND) ? iStart : m_Length;
+
+            char ch = m_Buffer[i];
+            m_Buffer[i] = '\0';
+            char *buf = strrchr(&m_Buffer[iStart], iCar);
+            m_Buffer[i] = ch;
+            if (buf)
+                return buf - m_Buffer;
+            else
+                return -1;
+        }
+        return -1;
+    }
 
     // creates a substring
-    VX_EXPORT XString Substring(XWORD iStart, XWORD iLength = 0) const;
+    XString Substring(XWORD iStart, XWORD iLength = 0) const
+    {
+        return XString(&m_Buffer[iStart], (iLength != 0) ? iLength : m_Length - iStart);
+    }
 
     // crops the string
-    VX_EXPORT XString &Crop(XWORD iStart, XWORD iLength);
+    XString &Crop(XWORD iStart, XWORD iLength)
+    {
+        if (iStart != 0)
+            strncpy(m_Buffer, &m_Buffer[iStart], iLength);
+        m_Length = iLength;
+        m_Buffer[iLength] = '\0';
+        return *this;
+    }
 
     // cuts the string
-    VX_EXPORT XString &Cut(XWORD iStart, XWORD iLength);
+    XString &Cut(XWORD iStart, XWORD iLength)
+    {
+        strncpy(&m_Buffer[iStart], &m_Buffer[iLength + iStart], m_Length - (iLength + iStart) + 1);
+        m_Length -= iLength;
+        return *this;
+    }
 
     // replaces all the occurrence of a character by another one
-    VX_EXPORT int Replace(char iSrc, char iDest);
+    int Replace(char iSrc, char iDest)
+    {
+        int count = 0;
+        for (XWORD i = 0; i < m_Length; ++i)
+        {
+            if (m_Buffer[i] == iSrc)
+            {
+                m_Buffer[i] = iDest;
+                ++count;
+            }
+        }
+        return count;
+    }
 
     // replaces all the occurrence of a string by another string
-    VX_EXPORT int Replace(const XBaseString &iSrc, const XBaseString &iDest);
+    int Replace(const XBaseString &iSrc, const XBaseString &iDest)
+    {
+        int count = 0;
+        char *src = NULL;
+
+        if (iSrc.m_Length == iDest.m_Length)
+        {
+            src = strstr(m_Buffer, iSrc.m_Buffer);
+            if (!src)
+                return 0;
+
+            do
+            {
+                strncpy(src, iDest.m_Buffer, iDest.m_Length);
+                ++count;
+            } while ((src = strstr(&src[iSrc.m_Length], iSrc.m_Buffer)));
+            return count;
+        }
+        else
+        {
+            src = strstr(m_Buffer, iSrc.m_Buffer);
+            if (src)
+            {
+                do
+                {
+                    ++count;
+                } while ((src = strstr(&src[iSrc.m_Length], iSrc.m_Buffer)));
+            }
+
+            XWORD len = m_Length + count * (iDest.m_Length - iSrc.m_Length);
+            char *buf = (char *)VxNew(sizeof(char) * (len + 1));
+
+            char *s1 = m_Buffer;
+            char *s2 = buf;
+
+            src = strstr(m_Buffer, iSrc.m_Buffer);
+            if (src)
+            {
+                do
+                {
+                    strncpy(s2, s1, src - s1);
+                    strncpy(&s2[src - s1], iDest.m_Buffer, iDest.m_Length);
+                    s2 = &s2[iDest.m_Length];
+                    s1 = &src[iSrc.m_Length];
+                } while ((src = strstr(s1, iSrc.m_Buffer)));
+            }
+            strncpy(s2, s1, (m_Buffer - s1) + 1);
+
+            VxDelete(m_Buffer);
+            m_Length = len;
+            m_Buffer = buf;
+            m_Allocated = len + 1;
+            return count;
+        }
+    }
 
     ///
     // Comparison operators
@@ -404,55 +711,120 @@ public:
     // Stream operators
 
     // Concatenation operator
-    VX_EXPORT XString &operator<<(const char *iString);
+    XString &operator<<(const char *iString)
+    {
+        if (iString)
+        {
+            Assign(iString, strlen(iString));
+        }
+        else
+        {
+            m_Length = 0;
+            if (m_Buffer)
+                m_Buffer[0] = '\0';
+        }
+        return *this;
+    }
 
     // Concatenation operator
-    VX_EXPORT XString &operator<<(const XBaseString &iString);
+    XString &operator<<(const XBaseString &iString)
+    {
+        CheckSize(m_Length + iString.m_Length);
+        if (iString.m_Length != 0)
+        {
+            strncpy(&m_Buffer[m_Length], iString.m_Buffer, iString.m_Length + 1);
+            m_Length += iString.m_Length;
+        }
+        return *this;
+    }
 
     // Concatenation operator
-    VX_EXPORT XString &operator<<(const char iValue);
+    XString &operator<<(const char iValue)
+    {
+        CheckSize(m_Length + 1);
+        m_Buffer[m_Length++] = iValue;
+        m_Buffer[m_Length] = '\0';
+        return *this;
+    }
 
     // Concatenation operator
-    VX_EXPORT XString &operator<<(const int iValue);
+    XString &operator<<(const int iValue)
+    {
+        char buf[16];
+        int len = sprintf(buf, "%d", iValue);
+        CheckSize(m_Length + len);
+        strncpy(&m_Buffer[m_Length], buf, len + 1);
+        m_Length += len;
+        return *this;
+    }
 
     // Concatenation operator
-    VX_EXPORT XString &operator<<(const unsigned int iValue);
+    XString &operator<<(const unsigned int iValue)
+    {
+        char buf[16];
+        int len = sprintf(buf, "%u", iValue);
+        CheckSize(m_Length + len);
+        strncpy(&m_Buffer[m_Length], buf, len + 1);
+        m_Length += len;
+        return *this;
+    }
 
     // Concatenation operator
-    VX_EXPORT XString &operator<<(const float iValue);
+    XString &operator<<(const float iValue)
+    {
+        char buf[16];
+        int len = sprintf(buf, "%f", iValue);
+        CheckSize(m_Length + len);
+        strncpy(&m_Buffer[m_Length], buf, len + 1);
+        m_Length += len;
+        return *this;
+    }
 
     // Concatenation operator
-    VX_EXPORT XString &operator<<(const void *iValue);
+    XString &operator<<(const void *iValue)
+    {
+        if (iValue)
+        {
+            XWORD len = (XWORD)strlen((char *)iValue);
+            if (len != 0)
+            {
+                CheckSize(m_Length + len);
+                strncpy(&m_Buffer[m_Length], (const char *)iValue, len + 1);
+                m_Length += len;
+            }
+        }
+        return *this;
+    }
 
     ///
     // + operators
 
-    VX_EXPORT XString operator+(const char *iString) const
+    XString operator+(const char *iString) const
     {
         XString tmp = *this;
         return tmp << iString;
     }
-    VX_EXPORT XString operator+(const XBaseString &iString) const
+    XString operator+(const XBaseString &iString) const
     {
         XString tmp = *this;
         return tmp << iString;
     }
-    VX_EXPORT XString operator+(const char iValue) const
+    XString operator+(const char iValue) const
     {
         XString tmp = *this;
         return tmp << iValue;
     }
-    VX_EXPORT XString operator+(const int iValue) const
+    XString operator+(const int iValue) const
     {
         XString tmp = *this;
         return tmp << iValue;
     }
-    VX_EXPORT XString operator+(const unsigned int iValue) const
+    XString operator+(const unsigned int iValue) const
     {
         XString tmp = *this;
         return tmp << iValue;
     }
-    VX_EXPORT XString operator+(const float iValue) const
+    XString operator+(const float iValue) const
     {
         XString tmp = *this;
         return tmp << iValue;
@@ -472,12 +844,23 @@ public:
     }
 
     // Sets the capacity of the string
-    VX_EXPORT void Reserve(XWORD iLength);
+    void Reserve(XWORD iLength)
+    {
+        if (iLength + 1 > m_Allocated)
+        {
+            m_Allocated = iLength + 1;
+            char *buf = (char *)VxNew(sizeof(char) * (iLength + 1));
+            if (m_Length != 0)
+                strncpy(buf, m_Buffer, m_Length);
+            VxDelete(m_Buffer);
+            m_Buffer = buf;
+        }
+    }
 
     void Resize(XWORD iLength)
     {
         Reserve(iLength);
-        if (iLength)
+        if (iLength != 0)
             m_Buffer[iLength] = 0;
         else if (m_Buffer)
             m_Buffer[0] = 0;
@@ -505,10 +888,36 @@ public:
 
 protected:
     // Check a string size to see if it fits in
-    void CheckSize(int iLength);
+    void CheckSize(int iLength)
+    {
+        if (iLength + 1 > m_Allocated)
+        {
+            m_Allocated = iLength + 1;
+            char *buf = (char *)VxNew(sizeof(char) * (iLength + 1));
+            if (m_Length != 0)
+                strncpy(buf, m_Buffer, m_Length + 1);
+            else
+                buf[0] = '\0';
+            VxDelete(m_Buffer);
+            m_Buffer = buf;
+        }
+    }
 
     // Assign a string to the current string
-    void Assign(const char *iBuffer, int iLength);
+    void Assign(const char *iBuffer, int iLength)
+    {
+        m_Length = 0;
+        if (iLength != 0)
+        {
+            CheckSize(iLength);
+            m_Length = iLength;
+            strncpy(m_Buffer, iBuffer, iLength);
+        }
+        else if (m_Buffer)
+        {
+            m_Buffer[0] = '\0';
+        }
+    }
 };
 
 #endif // XSTRING_H
