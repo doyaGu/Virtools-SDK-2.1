502d4f6a3f6b383d9c2a2b7d068864347afcffa3
doyaGu <doyaGu@users.noreply.github.com>
2022-10-10 14:03:54 +0800

    Update VxPlane

--- Include/VxPlane.h
+++ Include/VxPlane.h
@@ -218,9 +218,20 @@ public:
     ************************************************/
     const VxVector NearestPoint(const VxVector &p) const { return p + m_Normal * -Classify(p); }
 
-    VX_EXPORT void Create(const VxVector &n, const VxVector &p);
+    void Create(const VxVector &n, const VxVector &p)
+    {
+        m_Normal = n;
+        m_D = -DotProduct(n, p);
+    }
 
-    VX_EXPORT void Create(const VxVector &a, const VxVector &b, const VxVector &c);
+    void Create(const VxVector &a, const VxVector &b, const VxVector &c)
+    {
+        m_Normal = Normalize(VxVector(
+            (c.z - a.z) * (b.y - a.y) - (c.y - a.y) * (b.z - a.z),
+            (b.z - a.z) * (c.x - a.x) - (c.z - a.z) * (b.x - a.x),
+            (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y)));
+        m_D = -DotProduct(m_Normal, a);
+    }
 
     bool operator==(const VxPlane &iPlane) const
     {
