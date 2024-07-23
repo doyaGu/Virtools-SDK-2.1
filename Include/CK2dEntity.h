3631db5ec9df9bff3e24d6eb746f45a0e9054cc2
doyaGu <10153309+doyaGu@users.noreply.github.com>
2024-07-23 12:22:05 +0800

    Fix CK2dEntity

--- Include/CK2dEntity.h
+++ Include/CK2dEntity.h
@@ -397,15 +397,6 @@ public:
     *************************************************/
     virtual CK2dEntity *GetChild(int i) const CK_PURE;
 
-    /*************************************************
-    Summary: Checks if the object is root object.
-    Return Value:
-        TRUE if the entity is root, FALSE otherwise.
-    Remarks:
-        This method returns TRUE if the entity does not have a parent.
-
-    *************************************************/
-    virtual CKBOOL IsRootObject() CK_PURE;
     /************************************************
     Summary: Parses the sub hierarchy of an entity.
     Arguments:
